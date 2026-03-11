e9853f7cffa704dc3846ec3adb6f12e566b28b6b
a2022112989-cmyk <a2022112989@estudiantes.upsa.edu.bo>
2026-03-11 13:23:21 -0400

    Create Triangulo.cpp

---
+++ Triangulo.cpp
@@ -0,0 +1,12 @@
+#include "Triangulo.h"
+
+Triangulo::Triangulo() {
+
+}
+Triangulo::~Triangulo() {
+
+}
+
+float Triangulo::calculararea(float base, float altura) {
+	return (base * altura) / 2;
+}
