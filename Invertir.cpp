a53c04aac664facb79b31bcb80b4cafd94aaad4f
a2022112989-cmyk <a2022112989@estudiantes.upsa.edu.bo>
2026-03-11 13:42:32 -0400

    Create Invertir.cpp

---
+++ Invertir.cpp
@@ -0,0 +1,51 @@
+#include "StdAfx.h"
+#include <iostream>
+#include "vector.h"
+
+using namespace std;
+
+vector::vector(void)
+{
+	vec[10] = 0;
+	n = 0;
+}
+
+
+vector::~vector(void)
+{
+}
+
+void vector::cargarVector(int vec[], int n) {
+	for (int i = 0; i < n; i++) {
+		cout << "vec[" << i << "] =";
+		cin >> vec[i];
+	}
+}
+
+void vector::mostrarVector(int vec[], int n) {
+	for (int i = 0; i < n; i++) {
+		cout << vec[i] << ", ";
+	}
+	cout << endl;
+}
+
+void vector::ordenarVector(int vec[], int n) {
+	int aux;
+	for (int i = 0; i < (n - 1); i++) {
+		for (int j = i; j < n; j++) {
+			if (vec[i] > vec[j]) {
+				aux = vec[i];
+				vec[i] = vec[j];
+				vec[j] = aux;
+			}
+		}
+	}
+}
+
+int vector::invertirVector(int vec[], int n) {
+	int revertido[];
+	for (int i = 0; i < .n; i++) {
+		// El truco de la resta para invertir los índices
+		revertido.datos[i] = original.datos[original.n - 1 - i];
+	}
+}
